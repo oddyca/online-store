f0056bee0f15054393877dba7cd29035e004ae6c
Siarhei Prakapovich <prokser27@icloud.com>
2022-12-23 13:52:22 +0300

    feat: implement render of all products

---
+++ src/components/main/list/products.ts
@@ -0,0 +1,35 @@
+import { Route } from '../../router/route';
+import { FETCHED_DATA } from "../../data/data";
+
+const allProducts = Object.keys(FETCHED_DATA["products"]);
+
+export class ProductsList {
+  constructor() {
+
+  }
+
+  render(): HTMLDivElement {
+    const productsContainer = document.createElement('div');
+
+    for (let i = 0; i < allProducts.length; i++) {
+      const productCardLink = document.createElement('a');
+      const productCard = document.createElement('div');
+      const id = FETCHED_DATA['products'][i]["id"]
+
+      const productRoute = new Route(`/${id}`);
+
+      productCard.innerText = FETCHED_DATA['products'][i]["title"];
+      productCardLink.innerText = `${id}`;
+      productCardLink.setAttribute('href', `/${id}`)
+      productCardLink.onclick = (e):void => {
+        e.preventDefault();
+        productRoute.createRoute();
+      }
+
+      productCard.append(productCardLink);
+      productsContainer.append(productCard);
+    }
+
+    return productsContainer;
+  }
+}
