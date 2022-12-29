07756628f3a4e962ad7e6be5d5fa3fcc30bf3899
Olga <kartoshkaslukom@gmail.com>
2022-12-29 15:18:48 +0100

    feat: change display of products

--- src/components/main/list/products-header.ts
+++ src/components/main/list/products-header.ts
@@ -1,12 +1,16 @@
 import { FETCHED_DATA } from "../../data/data";
+import { Route } from '../../router/route';
 
 const sortingList = ['price', 'rating', 'discount'];
 const amount = FETCHED_DATA['products'].length;
+const listRoute = new Route(`/list`);
+const tableRoute = new Route(`/`);
+let isList = false;
 
 export class ProductsHeader {
-    amount: number
+    amount: number;
     constructor() {
-        this.amount = amount; // amount of found items
+        this.amount = amount;
     }
     render(): HTMLDivElement {
         const header = document.createElement('div');
@@ -30,10 +34,42 @@ export class ProductsHeader {
         header.appendChild(buttons);
         const button1 = document.createElement('button'); 
         button1.innerText = 'button 1';
+        button1.classList.add('button-list');
         buttons.appendChild(button1);
+        button1.addEventListener('click', () => {
+            if (isList) this.changeDisplay();
+        })
         const button2 = document.createElement('button');
         button2.innerText = 'button 2';
         buttons.appendChild(button2);
+        button2.addEventListener('click', () => {
+            if (!isList) this.changeDisplay(); 
+        })
         return header
     }
+    changeDisplay():void {
+        const products = document.querySelector('.products-list');     // change display of products
+        const itemsArr = document.querySelectorAll('.product-card');
+        const buttons = document.querySelectorAll('button');
+        if (isList) {
+            products?.classList.remove('display-list');
+            for (let i = 0; i < itemsArr.length; i++) {
+                itemsArr[i].classList.remove('card-list');
+            }
+            buttons[0].classList.add('button-list');
+            buttons[1].classList.remove('button-list');
+            tableRoute.createRoute();
+            isList = false;
+        } else {
+            products?.classList.add('display-list');
+            for (let i = 0; i < itemsArr.length; i++) {
+                itemsArr[i].classList.add('card-list');
+            }
+            buttons[1].classList.add('button-list');
+            buttons[0].classList.remove('button-list');
+            listRoute.createRoute();
+            isList = true;
+        }
+    }
 }
+ 
