5f8f681b3cea367aff59a5c21cfd14d7118b9777
Siarhei Prakapovich <prokser27@icloud.com>
2022-12-30 11:43:51 +0300

    refactor: refactor class syntax in main.ts

--- src/components/main/main.ts
+++ src/components/main/main.ts
@@ -3,9 +3,6 @@ import { CategoriesBlock, BrandsBlock } from './filter/filter';
 import { ProductsHeader } from './list/products-header';
 
 export class Main {
-  constructor() {
-
-  }
 
   render() {
     const mainSection = document.createElement('main');
@@ -13,14 +10,12 @@ export class Main {
     const filterBlock = document.createElement('div');
     filterBlock.classList.add('app_main_filters')
 
-    // TODO: create filter block that wraps categories, brand, price etc.
-    // only categories for now
-    const categoriesBlock = new CategoriesBlock();
-    const brandsBlock = new BrandsBlock();
-    const allProducts = new ProductsList();
+    const categoriesBlock = new CategoriesBlock;
+    const brandsBlock = new BrandsBlock;
+    const allProducts = new ProductsList;
     const products = document.createElement('div');
     products.classList.add('products');
-    const sortingList = new ProductsHeader();
+    const sortingList = new ProductsHeader;
 
     filterBlock.appendChild(categoriesBlock.render());
     filterBlock.appendChild(brandsBlock.render());
