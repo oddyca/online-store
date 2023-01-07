dd509f1f45b322b5169421743d37994200f0a966
Siarhei Prakapovich <prokser27@icloud.com>
2023-01-08 01:52:59 +0300

    feat: display cart in header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -9,10 +9,22 @@ export class Header {
 
     const priceCounter = document.createElement('div'); // IMPORT data from controller?
     priceCounter.classList.add('header_price-counter');
+    priceCounter.innerHTML = 'Cart total: '
+    const totalPrice = document.createElement('span');
+    totalPrice.classList.add('header_total-price');
+    totalPrice.innerHTML = '€0.00';
+    priceCounter.append(totalPrice);
 
     const cartElement = document.createElement('div'); // IMPORT from cart
     cartElement.classList.add('header_cart');
-    
+    const cartLogo = document.createElement('img');
+    cartLogo.setAttribute('src', require('../../assets/cart.svg'));
+    const cartItemCounter = document.createElement('div');
+    cartItemCounter.classList.add('cart_counter');
+    cartItemCounter.innerHTML = '0';
+    cartElement.append(cartLogo);
+    cartElement.append(cartItemCounter);
+
     headerElement.append(logoElement);
     headerElement.append(priceCounter);
     headerElement.append(cartElement);
